451e74c2369d7d3af1ca3323099dffecd09e618c
Court Schuett <schuettc@amazon.com>
2024-06-15 16:36:10 -0500

    fix: first project

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,57 @@
 import { cdk } from 'projen';
-const project = new cdk.JsiiProject({
+import { AwsCdkTypeScriptApp } from 'projen/lib/awscdk';
+const { JobPermission } = require('projen/lib/github/workflows-model');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
+const root = new cdk.JsiiProject({
   author: 'Court Schuett',
   authorAddress: 'schuettc@amazon.com',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
   name: 'anthropic-on-aws-dev',
   projenrcTs: true,
+  jest: false,
   repositoryUrl: 'https://github.com/schuettc/anthropic-on-aws-dev.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
-project.synth();
+
+new AwsCdkTypeScriptApp({
+  parent: root,
+  cdkVersion: '2.130.0',
+  defaultReleaseBranch: 'main',
+  outdir: 'cdk-demo',
+  name: 'cdk-demo',
+  devDeps: ['esbuild', 'cdk-nag'],
+  deps: ['dotenv', 'fs-extra', '@types/fs-extra', '@types/aws-lambda'],
+  appEntrypoint: 'claude-tools-chatbot.ts',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['schuettc'],
+  },
+  autoApproveUpgrades: true,
+});
+
+const common_exclude = [
+  '.yalc',
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+  '.DS_Store',
+  '.venv',
+  'src/resources/resolverLambda/bin/**',
+  'src/resources/resolverLambda/lib/**',
+  'src/resources/initializerLambda/bin/**',
+  'src/resources/initializerLambda/lib/**',
+  '.graphqlconfig.yaml',
+  'cdk-neg-output.txt',
+];
+
+root.gitignore.exclude(...common_exclude);
+root.synth();
