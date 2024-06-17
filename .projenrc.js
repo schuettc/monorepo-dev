46036361e561e620d03c6b638763305d6448ccc3
Court Schuett <schuettc@amazon.com>
2024-06-16 22:41:42 -0500

    feat: devcontainer

--- .projenrc.js
+++ .projenrc.js
@@ -1,44 +1,16 @@
-const { AwsCdkTypeScriptApp } = require('projen/lib/awscdk');
-const addUpgradeSiteWorkflow = require('./workflows.ts');
-const addBuildWorkflow = require('./workflows.ts');
 const { NodeProject } = require('projen/lib/javascript');
-const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+const cdkDemoConfig = require('./subprojects/cdk-demo');
 
 const root = new NodeProject({
   name: 'monorepo-dev',
   defaultReleaseBranch: 'main',
+  license: 'MIT-0',
+  author: 'Court Schuett',
+  copyrightOwner: 'Amazon.com, Inc.',
+  authorAddress: 'https://aws.amazon.com',
 });
 
-const cdkDemoProject = new AwsCdkTypeScriptApp({
-  parent: root,
-  cdkVersion: '2.130.0',
-  defaultReleaseBranch: 'main',
-  outdir: 'cdk-demo',
-  name: 'cdk-demo',
-  devDeps: ['esbuild', 'cdk-nag'],
-  deps: ['dotenv', 'fs-extra', '@types/fs-extra', '@types/aws-lambda'],
-  appEntrypoint: 'cdk-demo.ts',
-  depsUpgradeOptions: {
-    workflow: true,
-    workflowOptions: {
-      labels: ['auto-approve', 'auto-merge'],
-      schedule: UpgradeDependenciesSchedule.WEEKLY,
-    },
-  },
-  autoApproveOptions: {
-    secret: 'GITHUB_TOKEN',
-    allowedUsernames: ['schuettc'],
-  },
-  autoApproveUpgrades: true,
-  depsUpgrade: true,
-});
-
-cdkDemoProject.addTask(`launch:${cdkDemoProject.name}`, {
-  exec: 'yarn cdk bootstrap && yarn cdk deploy  --require-approval never && yarn configLocal',
-});
-
-root.addUpgradeSiteWorkflow('cdk-demo');
-root.addBuildWorkflow('cdk-demo');
+cdkDemoConfig(root);
 
 const common_exclude = [
   '.yalc',
@@ -53,23 +25,8 @@ const common_exclude = [
   'src/resources/initializerLambda/bin/**',
   'src/resources/initializerLambda/lib/**',
   '.graphqlconfig.yaml',
-  'cdk-neg-output.txt',
+  'cdk-nag-output.txt',
 ];
-
-cdkDemoProject.tsconfigDev.file.addOverride('include', ['src/*.ts']);
-
-// root.tsconfigDev.file.addOverride('include', [
-//   './.projenrc.ts',
-//   './workflows.ts',
-// ]);
-
-cdkDemoProject.eslint.addOverride({
-  files: ['./*.ts'],
-  rules: {
-    '@typescript-eslint/no-require-imports': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
-});
 
 root.gitignore.exclude(...common_exclude);
 root.synth();
---
+++ subprojects/cdk-demo.js
@@ -0,0 +1,48 @@
+const { AwsCdkTypeScriptApp } = require('projen/lib/awscdk');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+const addUpgradeSiteWorkflow = require('../workflows.ts');
+const addBuildWorkflow = require('../workflows.ts');
+
+module.exports = function (root) {
+  const cdkDemoProject = new AwsCdkTypeScriptApp({
+    parent: root,
+    cdkVersion: '2.130.0',
+    defaultReleaseBranch: 'main',
+    outdir: 'cdk-demo',
+    name: 'cdk-demo',
+    devDeps: ['esbuild', 'cdk-nag'],
+    deps: ['dotenv', 'fs-extra', '@types/fs-extra', '@types/aws-lambda'],
+    appEntrypoint: 'cdk-demo.ts',
+    depsUpgradeOptions: {
+      workflow: true,
+      workflowOptions: {
+        labels: ['auto-approve', 'auto-merge'],
+        schedule: UpgradeDependenciesSchedule.WEEKLY,
+      },
+    },
+    autoApproveOptions: {
+      secret: 'GITHUB_TOKEN',
+      allowedUsernames: ['schuettc'],
+    },
+    autoApproveUpgrades: true,
+    depsUpgrade: true,
+  });
+
+  cdkDemoProject.addTask(`launch:${cdkDemoProject.name}`, {
+    exec: 'yarn cdk bootstrap && yarn cdk deploy --require-approval never && yarn configLocal',
+  });
+
+  cdkDemoProject.tsconfigDev.file.addOverride('include', ['src/*.ts']);
+  cdkDemoProject.eslint.addOverride({
+    files: ['./*.ts'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  });
+
+  root.addUpgradeSiteWorkflow('cdk-demo');
+  root.addBuildWorkflow('cdk-demo');
+
+  return cdkDemoProject;
+};
