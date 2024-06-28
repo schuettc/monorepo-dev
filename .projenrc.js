1d9062ffb8c7088617fb45e342022df3dd3db499
Court Schuett <schuettc@amazon.com>
2024-06-28 01:54:09 -0500

    fix: update workflow

--- .projenrc.js
+++ .projenrc.js
@@ -27,6 +27,7 @@ const common_exclude = [
 ];
 
 const addWorkflow = root.github.addWorkflow('sns-publish');
+
 addWorkflow.on({
   push: {
     branches: ['main'],
@@ -43,6 +44,7 @@ addWorkflow.addJobs({
     permissions: {
       'contents': 'read',
       'id-token': 'write',
+      'pull-requests': 'read',
     },
     steps: [
       {
@@ -59,12 +61,40 @@ addWorkflow.addJobs({
         },
       },
       {
+        name: 'Generate Event Message',
+        id: 'generate-message',
+        run: `
+          echo "Generating detailed message based on event type"
+          if [[ "\${{ github.event_name }}" == "pull_request" ]]; then
+            echo 'message={
+              "event_type": "pull_request",
+              "action": "\${{ github.event.action }}",
+              "pr_number": "\${{ github.event.pull_request.number }}",
+              "pr_title": "\${{ github.event.pull_request.title }}",
+              "pr_body": "\${{ github.event.pull_request.body }}",
+              "author": "\${{ github.event.pull_request.user.login }}",
+              "base_branch": "\${{ github.event.pull_request.base.ref }}",
+              "head_branch": "\${{ github.event.pull_request.head.ref }}",
+              "repo_full_name": "\${{ github.repository }}",
+              "html_url": "\${{ github.event.pull_request.html_url }}",
+              "diff_url": "\${{ github.event.pull_request.diff_url }}",
+              "changed_files": "\${{ github.event.pull_request.changed_files }}",
+              "additions": "\${{ github.event.pull_request.additions }}",
+              "deletions": "\${{ github.event.pull_request.deletions }}"
+            }' >> $GITHUB_OUTPUT
+          else
+            echo 'message={"event_type": "\${{ github.event_name }}", "details": "Event details not configured for this type"}' >> $GITHUB_OUTPUT
+          fi
+        `,
+      },
+      {
         name: 'Publish to SNS',
         uses: 'schuettc/sns-action@v0.0.0',
         with: {
           topicArn: '${{ secrets.SNS_TOPIC_ARN }}',
-          message: 'Test message from consumer project',
-          subject: 'Test Notification',
+          message: '${{ steps.generate-message.outputs.message }}',
+          subject:
+            'GitHub Event: ${{ github.event_name }} in ${{ github.repository }}',
           region: 'us-east-1',
         },
       },
